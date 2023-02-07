a927af838b2fa09dea59e7b2b59c38cdc5fefeac
Kevin Floyd <kfloydy@gmail.com>
2023-02-06 19:37:44 -0600

    adding section on variables/constants

--- TheBasics.playground/Contents.swift
+++ TheBasics.playground/Contents.swift
@@ -1,3 +1,15 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+// Notes and Code from the basics chapter
+// Constants and Variables
+// The value of a constand can't be changed once it's set, whereas a variable can be set to a differnt value in the future.
+
+//Declaring Constand and variables
+
+let maximumNumberofLoginAttempts = 10 // contstant
+var currentLoginAttempts = 10 // variable
+var x = 0.0, y = 0.0, z = 0.0 // declaring multiple variables on a single line.
+
+// Type Annotation
+
+
