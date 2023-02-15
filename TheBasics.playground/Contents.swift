4b1c065df9f087236b4101b82c30509aa60fb5e9
Kevin Floyd <kfloydy@gmail.com>
2023-02-14 22:14:46 -0600

    adding section on Optionals

--- TheBasics.playground/Contents.swift
+++ TheBasics.playground/Contents.swift
@@ -138,3 +138,6 @@ print("The status code is \(http200Status.description)")
 
 //Tuples are particulary useful as the return values of functions
 
+// Optionals
+// You use optionals in situations where a value may be absent. An optional represents two possibilities. Either there is a value, and you can unwrap the optional to access that value, or there isn't a value at all.
+
